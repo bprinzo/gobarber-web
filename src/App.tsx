c976cd41d065abd7400024efe0f54499db795316
prinzo <brunoprinzo@hotmail.com>
2021-01-26 11:41:27 -0300

    component stylization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
+export default App;
